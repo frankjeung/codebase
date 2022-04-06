c30a82e5e5bdb539901bc9b5ff96f630c3c51388
frankjeung <97686278+frankjeung@users.noreply.github.com>
2022-04-06 11:49:56 -0400

    Testing (#4)
    * Test price formatting logic
    
    * Test the winner determination logic

--- app/utils.py
+++ app/utils.py
@@ -13,5 +13,30 @@ def to_usd(my_price):
     Example of invoking the function.
 
     Invoke like this: to_usd(9.9999)
+
+    Example return value "$9.99"
     """
     return '${:,.2f}'.format(my_price)
+
+
+
+
+## if this code is in the global scope
+## ... of a file we're trying to import from
+## ... it will throw errors when we try to run those other files
+#price = input("Please choose a price like 4.9999")
+#
+#print(to_usd(float(price)))
+
+
+if __name__ == "__main__":
+
+    # nesting code in the main condition will
+    # allow other scripts to cleanly import functions from this file
+    # without running this code
+
+    # this code still gets run when we invoke the script from the command line
+    price = input("Please choose a price like 4.9999")
+
+    print(to_usd(float(price)))
+    
---
+++ conftest.py
@@ -0,0 +1,6 @@
+
+# this os the"conftest.py" file
+
+# it can be empty
+
+# it helps pyest find our application code
